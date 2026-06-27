228dfdba5d21eaeff5663f11d0ea990668f75845
Moundistz <111893202+Moundistz@users.noreply.github.com>
2026-06-27 19:13:09 +0200

    Fix: preserve newlines in func_ov022_0211191c

--- src/func_ov022_0211191c.c
+++ src/func_ov022_0211191c.c
@@ -1,1 +1,7 @@
-void func_ov022_0211191c(char *a, char *b) {     int cond = *(unsigned short*)(b + 0xc);     cond = (cond == 0xbf);     if (cond) {         *(a + 0x324) = 1;     } }
+void func_ov022_0211191c(char *a, char *b) {
+    int cond = *(unsigned short*)(b + 0xc);
+    cond = (cond == 0xbf);
+    if (cond) {
+        *(a + 0x324) = 1;
+    }
+}
