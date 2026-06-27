ad8435b83ccd08930963df6fa0b074e7dc120f5f
Moundistz <111893202+Moundistz@users.noreply.github.com>
2026-06-27 19:13:15 +0200

    Fix: preserve newlines in func_ov034_02111588

--- src/func_ov034_02111588.c
+++ src/func_ov034_02111588.c
@@ -1,1 +1,7 @@
-extern unsigned char DecIfAbove0_Byte(unsigned char *p); extern void func_ov034_021125b8(char *c, int i);  void func_ov034_02111588(char *c) {     if (DecIfAbove0_Byte((unsigned char *)(c + 0x8da)) != 0) return;     func_ov034_021125b8(c, 0xa); }
+extern unsigned char DecIfAbove0_Byte(unsigned char *p);
+extern void func_ov034_021125b8(char *c, int i);
+
+void func_ov034_02111588(char *c) {
+    if (DecIfAbove0_Byte((unsigned char *)(c + 0x8da)) != 0) return;
+    func_ov034_021125b8(c, 0xa);
+}
