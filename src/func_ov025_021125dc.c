bf4d6a5d72cb7a814139b8236634bdef73cc6a54
ruspecial <alexsobolew7@gmail.com>
2026-07-10 03:55:14 -0400

    tangos(claude): matched work

--- src/func_ov025_021125dc.c
+++ src/func_ov025_021125dc.c
@@ -1,6 +1,6 @@
-extern void func_ov025_021125bc(char *a, char *b);
+extern void func_ov025_021125bc(int a, int b);
 
-void func_ov025_021125dc(char *self, char *a, char *b)
+void func_ov025_021125dc(int r0, int a, int b)
 {
     func_ov025_021125bc(a, b);
 }
